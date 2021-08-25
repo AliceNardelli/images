29fabd63bc60b3780802ca233f9e53a34c096404
AliceNardelli <alice.nardelli98@gmail.com>
2021-08-25 12:47:35 +0200

    alg update2

--- PeopleDetection.py
+++ PeopleDetection.py
@@ -9,9 +9,11 @@ import subprocess
 import cvlib as cv
 import os
 
+threshold=150
             
 def PeopleDetection():
     image1 = cv2.imread("/root/images/20.jpeg")
+
     #crop possibile
     faces1, confidences1 = cv.detect_face(image1)
     counter=len(confidences1)
@@ -20,25 +22,14 @@ def PeopleDetection():
 
         (startX,startY) = face[0],face[1]
         (endX,endY) = face[2],face[3]
-        #print(startX)
-        #print(endX)
-        print(endX-startX)
-        #print(startY)
-        #print(endY)
-        print(endY-startY)
-        print((endX-startX)*(endY-startY))
-        #print("   ###################           ")
-        #if endX-startX<
-        #draw rectangle over face
-        #controllo su y
-        if (endX-startX)*(endY-startY)>20000:
+        if (endY-startY)>threshold:
            cv2.rectangle(image1, (startX,startY), (endX,endY), (0,255,0), 2)
         else:
             counter-=1
 
     # save output
 
-    cv2.imwrite("/root/images/20new.jpeg", image1)
+    cv2.imwrite("/root/images/Inew.jpeg", image1)
     if counter>1:
         counter=1
     return counter
