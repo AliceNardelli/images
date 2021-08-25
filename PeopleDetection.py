ecd6b4ef98abfbcfd0aaaa11415cf7535426e201
AliceNardelli <alice.nardelli98@gmail.com>
2021-08-25 11:21:28 +0200

    alg modified

--- PeopleDetection.py
+++ PeopleDetection.py
@@ -11,29 +11,37 @@ import os
 
             
 def PeopleDetection():
-    image = cv2.imread("/root/images/table5.jpg")
-
-    # apply face detection
-    faces, confidences = cv.detect_face(image)
-
-    #print(faces)
-    #print(confidences)
-
+    image1 = cv2.imread("/root/images/20.jpeg")
+    #crop possibile
+    faces1, confidences1 = cv.detect_face(image1)
+    counter=len(confidences1)
     # loop through detected faces
-    for face,conf in zip(faces,confidences):
+    for face,conf in zip(faces1,confidences1):
 
         (startX,startY) = face[0],face[1]
         (endX,endY) = face[2],face[3]
-
-        # draw rectangle over face
-        cv2.rectangle(image, (startX,startY), (endX,endY), (0,255,0), 2)
+        #print(startX)
+        #print(endX)
+        print(endX-startX)
+        #print(startY)
+        #print(endY)
+        print(endY-startY)
+        print((endX-startX)*(endY-startY))
+        #print("   ###################           ")
+        #if endX-startX<
+        #draw rectangle over face
+        #controllo su y
+        if (endX-startX)*(endY-startY)>20000:
+           cv2.rectangle(image1, (startX,startY), (endX,endY), (0,255,0), 2)
+        else:
+            counter-=1
 
     # save output
-    cv2.imwrite("/root/images/P10.jpg", image)
-    if len(confidences)==0:
-        return 0
-    else:
-        return len(confidences)
+
+    cv2.imwrite("/root/images/20new.jpeg", image1)
+    if counter>1:
+        counter=1
+    return counter
 
 if __name__ == '__main__':
 
